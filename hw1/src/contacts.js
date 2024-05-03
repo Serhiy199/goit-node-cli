b82c80848514372a26b5755f44b92ab12af0df39
Sergiy Horodetskiy <bryklin.best.1994@gmail.com>
2024-05-04 01:52:01 +0300

    add-rem-cont

--- hw1/src/contacts.js
+++ hw1/src/contacts.js
@@ -1,22 +1,50 @@
-// contacts.js
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import crypto from 'node:crypto';
 
-/*
- * Розкоментуй і запиши значення
- * const contactsPath = ;
- */
+const contactsPath = path.resolve('db', 'contacts.json');
 
 async function listContacts() {
-  // ...твій код. Повертає масив контактів.
+    const arrContacts = await fs.readFile(contactsPath, { encoding: 'utf-8' });
+    return JSON.parse(arrContacts);
+}
+
+function writeContact(contacts) {
+    return fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
 }
 
 async function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+    const arrContacts = await listContacts();
+    const contact = arrContacts.find(contact => contact.id === contactId);
+
+    if (contact === undefined) {
+        return null;
+    }
+    return contact;
 }
 
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+    const arrContacts = await listContacts();
+
+    const index = arrContacts.findIndex(contact => contact.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    const removeContact = arrContacts[index];
+
+    const newArrContacts = [...arrContacts.slice(0, index), ...arrContacts.slice(index + 1)];
+
+    await writeContact(newArrContacts);
+
+    return removeContact;
 }
 
 async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту (з id).
+    const arrContacts = await listContacts();
+    const addContact = { id: crypto.randomUUID(), name, email, phone };
+    arrContacts.push(addContact);
+    await writeContact(arrContacts);
+    return addContact;
 }
+
+export default { listContacts, getContactById, removeContact, addContact };
--- hw1/src/index.js
+++ hw1/src/index.js
@@ -1,37 +1,41 @@
-import { program } from "commander";
+import { program } from 'commander';
+import contacts from './contacts.js';
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+    .option('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>', 'user email')
+    .option('-p, --phone <type>', 'user phone');
 
-program.parse();
+program.parse(process.argv);
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      // ...
-      break;
+    switch (action) {
+        case 'list':
+            const arrContacts = await contacts.listContacts();
+            console.table(arrContacts);
+            break;
 
-    case "get":
-      // ... id
-      break;
+        case 'get':
+            const getContactById = await contacts.getContactById(id);
+            console.table(getContactById);
+            break;
 
-    case "add":
-      // ... name email phone
-      break;
+        case 'add':
+            const addContact = await contacts.addContact(name, email, phone);
+            console.table(addContact);
+            break;
 
-    case "remove":
-      // ... id
-      break;
+        case 'remove':
+            const removeContact = await contacts.removeContact(id);
+            console.table(removeContact);
+            break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+    }
 }
 
 invokeAction(options);
